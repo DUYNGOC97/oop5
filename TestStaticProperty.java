f6eb5fd10f21b1579b2d28fa83e0b23b44197554
DUYNGOC97 <dinhphuongduyngocduno@gmail.com>
2023-04-16 20:33:23 +0700

    oop

---
+++ TestStaticProperty.java
@@ -0,0 +1,12 @@
+package car;
+
+public class TestStaticProperty {
+    public static void main(String[] args){
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        car1.display();
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+        car2.display();
+    }
+}
